a0eea421713673786324a3b5a8b28a0117f39725
ryanetunkel <ryantunk3l@gmail.com>
2023-02-18 13:53:49 -0500

    Button colors and more functionality

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -10,7 +10,7 @@
 let slidePosition = 0;
 
 // gather a reference to every slide we're using via the class name and querySelectorAll
-const slides = document.querySelectorAll('.carousel_item');
+const slides = document.querySelectorAll(".carousel_item");
 
 // change that "NodeList" into a Javascript "array", to get access to "array methods"
 const slidesArray = Array.from(slides);
@@ -20,19 +20,12 @@ const totalSlides = slidesArray.length;
 
 function updateSlidePosition() {
   slidesArray.forEach((slide) => {
-    slide.classList.remove('visible');
-    slide.classList.add('hidden');
+    slide.classList.remove("visible");
+    slide.classList.add("hidden");
   });
 
-  slides[slidePosition].classList.add('visible');
-
-  // Using the .forEach array method, (array.forEach((element) => { per-element work goes here }))
-  // loop through all the slides in your slideArray
-  // and remove the 'visible' class from each classList
-  // then add a class 'hidden' to all of them
-
-  // outside your .forEach,
-  // add a 'visible' class to the slide at the current slidePosition in slides
+  console.log(slidePosition);
+  slides[slidePosition].classList.add("visible");
 }
 
 function moveToNextSlide() {
@@ -42,6 +35,11 @@ function moveToNextSlide() {
     and if so, sets your slidePosition to the first index of an array
     if not, set the slidePosition to the current position plus one
   */
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition += 1;
+  }
   updateSlidePosition(); // this is how you call a function within a function
 }
 function moveToPrevSlide() {
@@ -52,6 +50,11 @@ function moveToPrevSlide() {
     and if so, sets your slidePosition to the last slide position in totalSlides
     if not, set the slidePosition to the current position minus one
   */
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition -= 1;
+  }
   updateSlidePosition();
 }
 
@@ -59,16 +62,20 @@ function moveToPrevSlide() {
   These two functions have been assigned via "addEventListener"
   to the elements accessed by the "querySelector" set to the class name on each
 */
-document.querySelector('.next') // Get the appropriate element (<button class="next">)
-  .addEventListener('click', () => { // set an event listener on it - when it's clicked, do this callback function
-    console.log('clicked next'); // let's tell the client console we made it to this point in the script
+document
+  .querySelector(".next") // Get the appropriate element (<button class="next">)
+  .addEventListener("click", () => {
+    // set an event listener on it - when it's clicked, do this callback function
+    console.log("clicked next"); // let's tell the client console we made it to this point in the script
     moveToNextSlide(); // call the function above to handle this
   });
 
 // Paying close attention to the above queryselector, write one that fires
 // when you want a "prev" slide
-document.querySelector('.prev') // Get the appropriate element (<button class="next">)
-  .addEventListener('click', () => { // set an event listener on it - when it's clicked, do this callback function
-    console.log('clicked prev'); // let's tell the client console we made it to this point in the script
+document
+  .querySelector(".prev") // Get the appropriate element (<button class="next">)
+  .addEventListener("click", () => {
+    // set an event listener on it - when it's clicked, do this callback function
+    console.log("clicked prev"); // let's tell the client console we made it to this point in the script
     moveToPrevSlide(); // call the function above to handle this
   });
