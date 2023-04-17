307bb972ec2616f093ed754b78dd84a777d90894
ryanetunkel <123666075+ryanetunkel@users.noreply.github.com>
2023-04-16 23:07:54 -0400

    Lab 8 Part 2

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -1,7 +1,3 @@
-/*
-  Hook this script to index.html
-  by adding `<script src="script.js">` just before your closing `</body>` tag
-*/
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -11,11 +7,11 @@ function getRandomIntInclusive(min, max) {
 function injectHTML(list) {
   console.log("fired injectHTML");
   const target = document.querySelector("#restaurant_list");
-  target.innerHTML = '';
+  target.innerHTML = "";
   list.forEach((item) => {
     const str = `<li>${item.name}</li>`;
-    target.innerHTML += str
-  })
+    target.innerHTML += str;
+  });
 }
 
 /* A quick filter that will return something based on a matching input */
@@ -28,18 +24,46 @@ function filterList(list, query) {
 }
 
 function cutRestaurantList(list) {
-  console.log("fired cut list");
-  // Array Method that does for-loop math to make code cleaner, easier to read, and use less reasoning
-  const range = [...Array(15).keys()] // ... is a destructuring element, makes an array of 15 elements that definitely only has 15 elements in it
-  return newArray = range.map((item) => { // map does same as foreach but returns new array
+  console.log("fired cut list");   // Array Method that does for-loop math to make code cleaner, easier to read, and use less reasoning
+  const range = [...Array(15).keys()]; // ... is a destructuring element, makes an array of 15 elements that definitely only has 15 elements in it
+  return (newArray = range.map((item) => { // map does same as foreach but returns new array
     const index = getRandomIntInclusive(0, list.length - 1);
-    return list[index]
-  })
+    return list[index];
+  }));
 }
 
-async function mainEvent() { // the async keyword means we can make API requests
+function initMap() {
+  // 38.9072 N, 77.0369 W
+  const carto = L.map('map').setView([38.98, -76.93], 13); // L means Use Leaflet
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(carto);
+  return carto;
+}
+
+function markerPlace(array, map) {
+  console.log("array for markers", array);
+
+  map.eachLayer((layer) => {
+    if (layer instanceof L.Marker) {
+      layer.remove();
+    }
+  });
+
+  array.forEach((item) => {
+    console.log("markerPlace", item);
+    const {coordinates} = item.geocoded_column_1;
+
+    L.marker([coordinates[1], coordinates[0]]).addTo(map);
+  });
+}
+
+async function mainEvent() { 
+  // the async keyword means we can make API requests
   const mainForm = document.querySelector(".main_form"); // This class name needs to be set on your form before you can listen for an event on it
   const loadDataButton = document.querySelector("#data_load");
+  const clearDataButton = document.querySelector("#data_clear");
   const generateListButton = document.querySelector("#generate");
   const textField = document.querySelector("#resto");
   
@@ -47,16 +71,19 @@ async function mainEvent() { // the async keyword means we can make API requests
   loadAnimation.style.display = "none";
   generateListButton.classList.add("hidden");
 
+  const carto = initMap();
+
   const storedData = localStorage.getItem("storedData");
-  const parsedData = JSON.parse(storedData);
-  if (parsedData.length > 0) {
+  let parsedData = JSON.parse(storedData);
+  if (parsedData?.length > 0) { // If we have parsed data, then check to see if have length, if not, pass right through
     generateListButton.classList.remove("hidden");
   }
 
   let currentList = []; // this is "scoped" to the main event function
   
   /* We need to listen to an "event" to have something happen in our page - here we're listening for a "submit" */
-  loadDataButton.addEventListener("click", async (submitEvent) => { // async has to be declared on every function that needs to "await" something 
+  loadDataButton.addEventListener("click", async (submitEvent) => { 
+    // async has to be declared on every function that needs to "await" something 
     console.log("Loading data"); // this is substituting for a "breakpoint" - it prints to the browser to tell us we successfully submitted the form
     loadAnimation.style.display = "inline-block";
 
@@ -67,8 +94,13 @@ async function mainEvent() { // the async keyword means we can make API requests
 
     // This changes the response from the GET into data we can use - an "object"
     const storedList = await results.json();
-    localStorage.setItem('storedData', JSON.stringify(storedList));
-    
+    localStorage.setItem("storedData", JSON.stringify(storedList));
+    parsedData = storedList;
+
+    if (parsedData?.length > 0) { 
+      generateListButton.classList.remove("hidden");
+    }
+
     loadAnimation.style.display = "none";
     // console.table(storedList); 
   });
@@ -76,16 +108,24 @@ async function mainEvent() { // the async keyword means we can make API requests
   generateListButton.addEventListener("click", (event) => {
     console.log("generate new list");
     currentList = cutRestaurantList(parsedData);
-    console.log(currentList)
+    console.log(currentList);
     injectHTML(currentList);
-  })
+    markerPlace(currentList, carto);
+  });
 
-  textField.addEventListener('input', (event) => {
-    console.log('input', event.target.value);
+  textField.addEventListener("input", (event) => {
+    console.log("input", event.target.value);
     const newList = filterList(currentList, event.target.value);
     console.log(newList);
     injectHTML(newList);
-  })
+    markerPlace(newList, carto);
+  });
+
+  clearDataButton.addEventListener("click", (event) => {
+    console.log("clear browser data");
+    localStorage.clear();
+    console.log("localStorage Check", localStorage.getItem("storedData"));
+  });
 }
 
 document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
